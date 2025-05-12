5afe30387499053d8ddb9b76bdcb2426007da517
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-05-12 18:50:08 +0700

    Membuat hitung.js
    Menghitung untuk logika penjumlahan, pengurangan, pembagian, dan perkalian dengan `hitung.js`.

---
+++ modules/hitung.js
@@ -0,0 +1,26 @@
+// modules/hitung.js
+
+import { memory } from '../memory.js';
+
+export function hitung(tokens) {
+    const varName = tokens[2].slice(1, -1);
+    const expressionTokens = tokens.slice(4);
+
+    let rawExpr = expressionTokens.join("");
+    if (rawExpr.startsWith("(") && rawExpr.endsWith(")")) {
+        rawExpr = rawExpr.slice(1, -1);
+    }
+
+    const replacedExpr= rawExpr.replace(/:\w+:/g, (match) => {
+        const name = match.slice(1, -1);
+        const value = memory[name];
+        return value !== undefined ? value : "0";
+    });
+
+    try {
+        const result = eval(replacedExpr);
+        memory[varName] = result;
+    } catch (err) {
+        console.error("Gagal menghitung: ", err.message);
+    }
+}
