16ed89bf3d017332fcbae8a3c0863d3a2a2666c7
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-05-14 18:52:57 +0700

    Agar tampilan mendukung punggung di kelas
    Tampilan mendukung di modul kelas yang baru saja dikomit (282aa2c) atas perubahan baru yaitu punggung agar kompatibel di kasus dimana tampilan digunakan.

--- modules/tampilkan.js
+++ modules/tampilkan.js
@@ -2,11 +2,23 @@
 
 import { memory } from '../memory.js';
 
-export function tampilkan(tokens) {
+export async function tampilkan(tokens, modules, context) {
     const target = tokens[1];
+
     if (target.startsWith(":")) {
         const varName = target.slice(1, -1);
         console.log(memory[varName] || "tidak dikenali");
+    } else if (target.include(".")) {
+        const [className, attrName] = target.split(".");
+        const kelas = memory[className];
+
+        if (kelas && kelas.__tipe === "kelas") {
+            const value = kelas.instance[attrName];
+            console.log(value ?? `atribut '${attrName}' tidak ditemukan di kelas '${className}'`);
+        } else {
+            console.log(`Kelas '${className}' tidak ditemukan`);
+        }
+
     } else {
         console.log(target.replace(/"/g, ""));
     }
