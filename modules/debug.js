f00115433e01d22de2e183efd06d2cf07d8dd624
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-05-15 15:58:17 +0700

    Membuat debug.js
    Modul `debug.js` ditambahkan ketika ada pengecekan lebih lanjut tentang masalah serangga di baris kode Anda yang tersimpan di `memory`.

---
+++ modules/debug.js
@@ -0,0 +1,18 @@
+const { memory } = require('../memory.js');
+
+async function debug(tokens, modules, context) {
+  const arg = tokens[1];
+
+  if (!arg || arg === memory) {
+    console.log('Isi mempry saat ini', JSON.stringify(memory, null, 2));
+  } else if (arg === 'context') {
+    console.log('Konteks eksekusi:', {
+      index: context.index,
+      sisa_baris: context.lines.length - context.index
+    });
+  } else {
+    console.warn(`Argumen debug tidak dikenali: '${arg}'`);
+  }
+}
+
+module.exports = { debug };
