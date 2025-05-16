3f1b0a492afb39c3bcafae6b5f4f6afb33412138
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-05-16 18:21:21 +0700

    Pengaruh perubahan untuk `kelas.js`
    Mneambahkan `resolve` dengan `Promise` untuk kaitan hubungan baru dengan kelas.

--- modules/masukkan.js
+++ modules/masukkan.js
@@ -3,39 +3,51 @@
 const { memory } = require('../memory.js');
 const readline = require('readline');
 
-const rl = readline.createInterface({
-    input: process.stdin;,
-    output: process.stdout
-});
-
 function masukkan(tokens) {
-    if (tokens[1].startsWith(':') && tokens[1].endsWith(':') && tokens[2] === 'sebagai') {
-        const namaKelas = tokens[1].replace(/:/g, '');
-        const namaInstance = tokens[3];
+    return new Promise((resolve) => {
+        if (
+            tokens[1].startsWith(':') &&
+            tokens[1].endsWith(':') &&
+            tokens[2] === 'sebagai'
+        ) {
+            const namaKelas = tokens[1].replace(/:/g, '');
+            const namaInstance = tokens[3];
 
-        if (!memory[namaKelas]) {
-            console.error(`Kelas ${namaKelas} belum didefinisikan.`);
+            if (!memory[namaKelas]) {
+                console.error(`Kelas ${namaKelas} belum didefinisikan.`);
+                return resolve();
+            }
+
+            if (memory[namaKelas].__tipe !== 'kelas') {
+                console.error(`'${namaKelas}' bukan kelas.`);
+                return resolve();
+            }
+
+            const instance = {};
+            for (const attr of memory[namaKelas].atribut || []) {
+                instance[attr] = null;
+            }
+
+            memory[namaInstance] = {
+                __tipe: namaKelas,
+                ...instance,
+            };
+            console.log(`Instance ${namaInstance} dari kelas ${namaKelas} dibuat.`);
             return;
         }
 
-        const instance = {};
-        for (const attr of memory[namaKelas].atribut) {
-            instance[attr] = null;
-        }
+        const varName = tokens[1].replace(/:/g, '');
+        const rl = realine.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
 
-        memory[namaInstance] = {
-            __tipe: namaKelas,
-            ...instance
-        };
-        console.log(`Instance ${namaInstance} dari kelas ${namaKelas} dibuat.`);
-        return;
-    }
-
-    const varName = tokens[1].slice(1, -1);
-    rl.question(`Masukkan nilai untuk ${varName}:` (userInput) => {
-        memory[varName] = userInput;
-        console.log(`Nilak untuk ${varName} disimpan: ${userInput}`);
-        rl.close();
+        rl.question(`Masukkan nilai untuk '${varName}':`, (userInput) => {
+            memory[varName] = userInput;
+            console.log(`Nilai untuk ${varName} disimpan: ${userInput}`);
+            rl.close();
+            resolve();
+        });
     });
 }
 
