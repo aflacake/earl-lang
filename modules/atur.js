a8fee6c2c8cef7b954d662fbb216e0794f750953
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-05-16 18:30:34 +0700

    Pengaruh perubahan untuk `kelas.js`
    Perubahan untuk kelas dipengaruhi oleh setiap modul yang ikut terpengaruh termasuk `atur.js` yang mengatur variable.

--- modules/atur.js
+++ modules/atur.js
@@ -40,18 +40,29 @@ function atur(tokens) {
     const [namaInstance, namaAtribut] = path.split('.');
 
     if (!namaAtribut) {
-        console.error(`Format target '${path}' tidak valid. Gunakan :var: atau Kelas.attr`);
+        console.error(`Format target '${path}' tidak valid. Gunakan :var: atau instance.atribut`);
         return;
     }
 
     const instance = memory[namaInstance];
-    if (!instance || instance.__tipe !== 'kelas') {
-        console.error(`Instance '${namaInstance}' tidak ditemukan atau bukan kelas.`);
+    if (!instance || instance.__tipe || !memory[instance.__tipe]) {
+        console.error(`Instance '${namaInstance}' tidak ditemukan atau tidak memiliki kelas.`);
         return; 
     }
     
+    const classDef = memory[instance.__tipe];
+
+    if (classDef.__tipe !== 'kelas') {
+        console.error(`'${instance.__tipe}' bukan kelas yang valid.`);
+        return;
+    }
+
+    if (!classDef.attribut.includes(namaAtribut)) {
+        console.error(`Atribut '${namaAtribut}' tidak didefinisikan dalam kelas '${instance.__tipe}'.`)
+    }
+
     instance.instance[namaAtribut] = value;
-    console.log(`${namaInstance}.${namaAtribut} diatur ke`, value);
+    console.log(`${namaInstsance}.${namaAtribut} diatur ke`, value);
 }
 
 module.exports = { atur };
