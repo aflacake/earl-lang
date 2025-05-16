9785cf423c59b0f59741b0e0d844ee977fa7b40f
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-05-16 12:30:58 +0700

    Konteks untuk debug
    Pengguna dapat mengatur untuk mendebug memori dengan memilih opsi hanya `memory` saja atau konteksnya, atau semuanya yang di debug dengan menjalakan perintah `debug <PERINTAH_YANG_DIPILIH>`.

--- modules/debug.js
+++ modules/debug.js
@@ -5,13 +5,27 @@ const { memory } = require('../memory.js');
 async function debug(tokens, modules, context) {
   const arg = tokens[1];
 
-  if (!arg || arg === memory) {
-    console.log('Isi mempry saat ini', JSON.stringify(memory, null, 2));
+  if (!arg || arg === 'memory') {
+    console.log('=== DEBUG MEMORY ===');
+    console.log('Isi memory saat ini', JSON.stringify(memory, null, 2));
   } else if (arg === 'context') {
+    console.log('=== DEBUG CONTEXT ===');
     console.log('Konteks eksekusi:', {
       index: context.index,
-      sisa_baris: context.lines.length - context.index
+      total_baris: context.lines.length;
+      baris_saat_ini: context.lines[context.index]
     });
+  } else if (arg === 'semua') {
+    console.log('=== DEBUG MEMORY ===');
+    console.log('Isi memory saat ini', JSON.stringify(memory, null, 2));
+
+    console.log('=== DEBUG CONTEXT ===');
+    console.log('Konteks eksekusi:', {
+      index: context.index,
+      total_baris: context.lines.length;
+      baris_saat_ini: context.lines[context.index]
+    });
+
   } else {
     console.warn(`Argumen debug tidak dikenali: '${arg}'`);
   }
