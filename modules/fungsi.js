13d40cc28deccce3c3867ebbe6b35ba254724be2
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-05-20 19:37:26 +0700

    Menambahkan sub-modul beserta kaitkan dengan modul lain
    Sub-modul yaitu fungsi dalam fungsi beserta juga modul kaitannya dengan kembalikan, komitmen selanjutnya pembuatan menyusul.

--- modules/fungsi.js
+++ modules/fungsi.js
@@ -9,24 +9,63 @@ function fungsi (tokens, modules, context) {
     throw new Error("Fungsi harus diikuti blok kode tanda kurung ()");
   }
   
+  let depth = 1;
   context.index++;
+
+
   while(context.index < context.lines.length) {
     const line = context.lines[context.index].trim();
-    if (line === ')') break;
-    body.push.(line);
+    if (line === ')') {
+        depth++;
+    } else if (line === ')') {
+        depth--;
+        if (depth === 0) {
+            break;
+        }
+    }
+
+    body.push(line);
     context.index++;
   }
 
-  modules[namaFungsi] = async () => {
-    for (let line of body) {
+  modules[namaFungsi] = async (tokens, modules, parentContext) => {
+    const args = tokens.slice(1);
+    const localContent = {
+        index: 0;
+        lines: body,
+        vars: {};
+        return: null,
+        stopExecution: false,
+        ...parentContext,
+    };
+
+    params.forEach((param, i) => {
+        localContext.vars[param] = args[i] || '';
+    });
+
+    while (localContent.index < localContext.lines.length) {
+      const line = localContent.lines[localContent.index].trim();
       const tokens = modules.tokenize(line);
-      if (tokens.length === 0) continue;
-      const cmd = tokens[0];
+      if (!tokens || tokens.length === 0) {
+          localContent.index++;
+          continue;
+      }
+
+      const evaluatedTokens = tokens.map(t => {
+        return localContext.vars[t] !== undefined ? localContext.vars[t] : t;
+      });
+
+      const cmd =  evaluatedTokens[0];
 
       if (modules[cmd]) {
-        await modules[cmd](tokens, modules, context);
+        await modules[cmd]( evaluatedTokens, modules, localContext);
+      } else {
+        console.error('Perintah '${cmd}' tidak dikenali dalam fungsi '${namaFungsi}'`)
       }
+      if (localContext.stopExecution) break;
+      localContext.index++;
     }
+    return localContext.return;
   };
 }
 
