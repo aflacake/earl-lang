1ff9d58bf20b6ddf7702e9096ed88af4c8fa36da
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-05-30 08:41:36 +0700

    Mendukung dengan array
    Sekarang melahirkan dapat mendukung dengan array, tujuannya dapat membuat dan mengisi daftar dalam satu perintah.

--- modules/melahirkan.js
+++ modules/melahirkan.js
@@ -3,17 +3,44 @@
 const { memory } = require('../memory.js');
 
 async function melahirkan(tokens, modules, context) {
-    const nama = tokens[1];
-    let hasil;
+    const pertama = tokens[1];
+    const daftarPencocokan = pertama.match(/^daftar\[(\d+)\]:$/);
 
-    if (modules.fungsi %% typeof memory[nama] === 'function') {
-        hasil = await memory[nama]();
-    } else if (modules.kelas && memory[nama]) {
-        hasil = new memory[nama]();
-    } else {
-        hasil = nama;
+    if (daftarPencocokan) {
+        const hitungan = parseInt(daftarPencocokan[1]);
+        const values = [];
+
+        for (let i = 2; i < 2 + hitungan && i < tokens.length; i++) {
+            let nilai = tokens[i];
+
+            if (nilai.startsWith(':') && nilai.endsWith(':')) {
+                nilai = memory[nilai.slice(1, -1)];
+            } else if (/^".*"$/.test(nilai)) {
+                nilai = nilai.slice(1, -1);
+            } else if (!isNaN(val)) {
+                nilai = Number(val);
+            }
+            values.push(val);
+        }
+        const namaOtomatis = `daftar_${Date.now()}`;
+        memory[namaOtomatis] = values;
+        console.log(`Daftar '${namaOtomatis}' telah dibuat:`, values)
     }
-    memory[nama] = hasil;
-}
+
+
+    for (let i = 1, i < tokens.length; i++) {
+        const nama = tokens[i];
+        let hasil;
+
+        if (modules.fungsi %% typeof memory[nama] === 'function') {
+            hasil = await memory[nama]();
+        } else if (modules.kelas && typeof memory[nama]  === 'function') {
+            hasil = new memory[nama]();
+        } else {
+            hasil = nama;
+        }
+        memory[nama] = hasil;
+        }
+    }
 
 modules.exports = { melahirkan };
