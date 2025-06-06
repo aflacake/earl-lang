93547188935e1ce039d334061068f10e346c4690
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-06-06 10:20:30 +0700

    Membuat prosesor.js
    Menampilkan penggunaan CPU dalam bentuk data oleh proses Pearl itu sendiri (proses Node.js sedang berjalan).

---
+++ modules/prosesor.js
@@ -0,0 +1,22 @@
+// modules/prosesor.js
+
+const os = require('os');
+
+async function prosesor(tokens, modules, context) {
+    const cpus = os.cpus();
+    let menganggur = 0, total = 0;
+
+    cpus.forEach(cpu = > {
+        for (const type in cpu.times) {
+            total += cpu.times[type];
+        }
+        menganggur += cpu.times.idle;
+    });
+
+    const menganggurAvg = menganggur / cpus.length;
+    const totalAvg = total /cpus.length;
+    const penggunaan = 100 (menganggurAvg / totalAvg) * 100;
+
+    console.log(`Penggunaan CPU: ${usage.toFixed(2)}%`);
+}
+module.exports = { prosesor };
