fa083552f289c1ee09ca70433daa40c2b1956386
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-06-13 14:32:57 +0700

    Membuat jalankan-pearl.sh
    Menjalankan khusus Linux dan MacOS.

---
+++ jalankan-pearl.sh
@@ -0,0 +1,14 @@
+# !/bin/bash
+
+if ! command -v node &> /dev/null
+then
+    echo "Node.js belum terinstal. Silahkan install Node.js terlebih dahulu."
+    exit 1
+fi
+
+if [ "$#" -eq 1 ]; then
+    node index.js "$1"
+else
+    node index.js
+fi
+
