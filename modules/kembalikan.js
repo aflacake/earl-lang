3889f5ce91bb782c381538cf7eaa9e0362c05b9c
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-06-21 09:23:47 +0700

    Mneyesuaikan dengan modul fungsi dan tampilkan
    Modul `fungsi.js` mendukung dengan kembalikan sedangkan butuh penyesuaian dengan modul `tampilkan.js`. Seklarang sesuai dengan mengimpor `resolveToken` dan `evalMathExpression` yang aman.

--- modules/kembalikan.js
+++ modules/kembalikan.js
@@ -1,8 +1,24 @@
 // modules/kembalikan.js
 
+const { resolveToken, evalMathExpression } = require('./tampilkan');
+
 async function kembalikan(tokens, modules, context) {
-    const hasil = tokens.slice(1).join('');
-    context.return = hasil;
+    const exprTokens = tokens.slice(1);
+
+    if (exprTokens.length === 0) {
+        context.return = undefined;
+    } else if (exprTokens.length === 1) {
+        context.return = resolveToken(exprTokens[0]);
+    } else {
+        const expr = exprTokens.join(' ');
+        const hasilEvaluasi = evalMathExpression(expr);
+
+        if (typeof hasilEvaluasi === 'number' && !isNaN(hasilEvaluasi)) {
+            context.return = hasilEvaluasi;
+        } else {
+            context.return = expr;
+        }
+    }
     context.stopExecution = true;
 }
 
