bf6b606e1d43f2614502c383fcc0792e6ac08aa0
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-06-24 14:19:29 +0700

    Kesalahan mengambil variabel
    Variabel `used` seharusnya `digunakan`.

--- modules/ingatan.js
+++ modules/ingatan.js
@@ -6,7 +6,7 @@ async function ingatan(tokens, modules, context) {
     const total = os.totalmem() / 1024 / 1024;
     const sisa = os.freemem() /1024 / 1024;
     const digunakan = total - sisa;
-    console.log(`Penggunaan memori saat ini: ${used.toFixed(2)} MB dari total ${total.toFixed(2)} MB`);
+    console.log(`Penggunaan memori saat ini: ${digunakan.toFixed(2)} MB dari total ${total.toFixed(2)} MB`);
 }
 
 module.exports = { ingatan };
