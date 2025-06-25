6fa75162738394e302fc273c6a8c854129a29170
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-06-26 06:19:49 +0700

    Kesalahan struktur
    Ada dublikasi bagian atas dan kemungkinan file tidak selesai ditulis. Hal ini menyebabkan merusak sintaks dan membuat file tidak valid.

--- modules/aturheader.js
+++ modules/aturheader.js
@@ -8,16 +8,6 @@ function aturheader(tokens) {
         return;
     }
 
-    const kunci = tokens[1].replace// modules/aturheader.js
-
-const { memory } = require('../memory.js');
-
-function aturheader(tokens) {
-    if(tokens.length < 3) {
-        console.error('Format: aturheader "Header-Nama" nilai');
-        return;
-    }
-
     const kunci = tokens[1].replace(/^"+|"+$/g, '');
     const nilai = tokens.slice(2).join(' ').replace(/^"+|"+$/g, '');
 
