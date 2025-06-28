0ef0f5763b61709998b1c1a505dc03b15db38ad0
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-06-28 10:19:16 +0700

    Parser dan pelaksana
    _Refactor_`runEarl()` agar dipakai di parser tersebut (parser.js).

--- index.js
+++ index.js
@@ -33,9 +33,14 @@ function bantuan() {
     console.log("Ketik 'keluar' untuk keluar dari mode REPL.");
 }
 
+const { parse } = require('./parser');
+const { laksanakanAST } = require('./pelaksana-ast')
+
 async function runEarl(code, customModules = modules, parentContext) {
     const lines = code.trim().split('\n');
-    const context = parentContext ?? { index: 0, lines, lingkup: [{}] };
+    const ast = parse(code);
+    const context = parentContext ?? { index: 0, lines: [], lingkup: [{}] };
+    await laksanakanAST(ast, customModules, context);
 
     if (!parentContext) context.lines = lines;
 
