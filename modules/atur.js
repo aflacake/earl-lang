34eab8c34e1ec2efa3e4681fd972cd91228d46f2
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-06-28 19:29:10 +0700

    Memecah token literal objek
    Perlu memecahkan token literal objek ke dalam token individual sebelum parsing. Solusinya gunakan tokenizer untuk memecah uang string `valueRaw` sebelum dikirim ke `parseObjekValue()`.

--- modules/atur.js
+++ modules/atur.js
@@ -96,7 +96,9 @@ async function atur(tokens, modules, context) {
     if (valueTokens[0] === '[' && valueTokens[valueTokens.length - 1] === ']') {
         value = parseArrayValue(valueTokens);
     } else if (valueTokens[0] === '(' && valueTokens[valueTokens.length - 1] === ')') {
-       value = parseObjekValue(valueTokens);
+        const objekString = valueTokens.join(' ');
+        const objekTokens = modules.tokenize(objekString);
+        value = parseObjekValue(objekTokens);
     } else {
         const valueRaw = valueTokens.join('').trim();
         value = resolveToken(valueRaw, {
