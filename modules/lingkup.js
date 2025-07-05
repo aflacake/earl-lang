85f315d67b6764f7da6e7eee16a9a40f29055e10
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-07-05 07:31:20 +0700

    Memastikan variabel diakses lingkup global
    Variabel dapat diakses setelah keluar dari lingkup, memeriksa variabel di lingkup global terlebih dahulu setelah keluar dari lingkup lokal, ini perubahan sedikit kode bagian `keluarlingkup` untuk memastikan variabel tetap bisa diakses lingkup global.

--- modules/lingkup.js
+++ modules/lingkup.js
@@ -15,7 +15,8 @@ async function keluarlingkup(tokens, modules, context) {
     }
 
     if (context.lingkup.length > 1) {
-        context.lingkup.pop();
+        const lingkupKeluar = context.lingkup.pop();
+        Object.assign(context.lingkup[0], lingkupKeluar);
     } else {
         console.warn('Tidak bisa keluar dari lingkup global.');
     }
