862d065355dbc3ba20f8e851793ff31bbdb06059
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-07-20 18:36:41 +0700

    Tunduk pada format atur
    Memperbaiki modul `membangun.js` dan cara penggunaan agar bisa menerima format tersebut dengan ubah sedikit cara modul menerima input.

--- modules/membangun.js
+++ modules/membangun.js
@@ -5,44 +5,44 @@ const { memory } = require('../memory.js');
 function membangun(tokens) {
     const varName = tokens[1].slice(1, -1);
 
-    if (tokens[2] === 'dari') {
-        const dari = tokens[3].replace(/"/g, '');
-        const sampai = tokens[5].replace(/"/g, '');
+    if (tokens[2] === '=' && tokens[3] === 'dari') {
+        const dari = tokens[4].replace(/"/g, '');
+        const sampai = tokens[6].replace(/"/g, '');
 
         if (!isNaN(dari) && !isNaN(sampai)) {
-            const hasil = [];
+            let hasil = [];
             let start = parseInt(dari);
             let end = parseInt(sampai);
 
-            if (start > end) {
-                for (let i = start; i >= end; i--) {
-                    hasil.push(i);
-                }
-            } else {
+            if (start <= end) {
                 for (let i = start; i <= end; i++) {
                     hasil.push(i);
                 }
+            } else {
+                for (let i = start; i >= end; i--) {
+                    hasil.push(i);
+                }
             }
+
             memory[varName] = hasil;
-        } 
-        else if (dari.length === 1 && sampai.length === 1) {
-            const hasil = [];
+        } else if (dari.length === 1 && sampai.length === 1) {
+            let hasil = [];
             let start = dari.charCodeAt(0);
             let end = sampai.charCodeAt(0);
 
-            if (start > end) {
-                for (let i = start; i >= end; i--) {
-                    hasil.push(String.fromCharCode(i));
-                }
-            } else {
+            if (start <= end) {
                 for (let i = start; i <= end; i++) {
                     hasil.push(String.fromCharCode(i));
                 }
+            } else {
+                for (let i = start; i >= end; i--) {
+                    hasil.push(String.fromCharCode(i));
+                }
             }
+
             memory[varName] = hasil;
-        }
-        else {
-            console.error("Format membangun tidak dikenali.");
+        } else {
+            console.error("Format rentang tidak dikenali.");
         }
     }
 }
