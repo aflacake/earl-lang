7ff5b5abb33725d83d4ea00a09d5252a5df057af
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-07-26 12:12:45 +0700

    Mengganti versi
    Ke yang lebih terkini.

--- modules/versi.js
+++ modules/versi.js
@@ -1,7 +1,7 @@
 // modules/versi.js
 
 function versi(tokens, modules, context) {
-    console.log('Earl Bahasa Pemrograman Alur Kerja versi 2.0.2');
+    console.log('Earl Bahasa Pemrograman Alur Kerja versi 2.0.6');
 }
 
 module.exports = { versi };
