cbaf44f2a7982bf1c866acb77573d59ca2b876cb
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-08-02 08:40:12 +0700

    Menaikkan nilai
    Menaikkan nilai `score` setiap objek di dalam _array_ `students`

--- modules/mengangkat.js
+++ modules/mengangkat.js
@@ -8,17 +8,11 @@ function naikkanNilai(tokens, context) {
         return;
     }
 
-    const namaVarToken = tokens[1];
-    if (!namaVarToken.startsWith(':') || !namaVarToken.endsWith(':')) {
-        console.error("Nama variabel harus dalam format :nama:");
-        return;
-    }
-
-    const namaVar = namaVarToken.slice(1, -1);
+    const namaVar = tokens[1];
     const meningkatSebesarEksp = tokens.slice(2).join(' ');
 
-    const nilaiSaatIni = resolveToken(`:${namaVar}:`, context);
-    const meningkatSebesar = evalMathExpression(meningkatSebesarEksp);
+    const nilaiSaatIni = resolveToken(namaVar, context);
+    let meningkatSebesar = evalMathExpression(meningkatSebesarEksp);
 
     if (isNaN(meningkatSebesar)) {
         console.error(`Nilai yang akan ditambahkan (${meningkatSebesarEksp}) tidak valid.`);
@@ -27,11 +21,24 @@ function naikkanNilai(tokens, context) {
 
     if (typeof nilaiSaatIni === 'number') {
         context.memory[namaVar] = nilaiSaatIni + meningkatSebesar;
-        console.log(`Variabel '${namaVar}' dinaikkan menjadi ${context.memory[namaVar]}`);
+        console.log(`Variabel '${namaVar}' dinaikkan menjadi ${nilaiSaatIni + meningkatSebesar}`);
     } else if (Array.isArray(nilaiSaatIni)) {
-        nilaiSaatIni.push(meningkatSebesar);
-        context.memory[namaVar] = nilaiSaatIni;
-        console.log(`Elemen baru ${meningkatSebesar} ditambahkan ke daftar atau array '${namaVar}'`);
+        // Cek apakah array elemen objek
+        if (nilaiSaatIni.length > 0 && typeof nilaiSaatIni[0] === 'object' && nilaiSaatIni[0] !== null) {
+            for (const obj of nilaiSaatIni) {
+                for (const kunci in obj) {
+                    if (typeof obj[kunci] === 'number') {
+                        obj[kunci] += meningkatSebesar;
+                        console.log(`Atribut '${kunci}' pada objek dinaikkan menjadi ${obj[kunci]}`);
+                    }
+                }
+            }
+            context.memory[namaVar] = nilaiSaatIni;
+        } else {
+            nilaiSaatIni.push(meningkatSebesar);
+            context.memory[namaVar] = nilaiSaatIni;
+            console.log(`Elemen baru ${meningkatSebesar} ditambahkan ke daftar atau array '${namaVar}'`);
+        }
     } else if (typeof nilaiSaatIni === 'object' && nilaiSaatIni !== null) {
         for (const kunci in nilaiSaatIni) {
             if (typeof nilaiSaatIni[kunci] === 'number') {
