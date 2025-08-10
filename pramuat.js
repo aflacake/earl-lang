32d2c0c06e5b193e383a3b3027250dfa3f0acf1d
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-08-10 10:03:21 +0700

    Menyesuaikan dengan `apg`
    Sudah disesuaikan, hanya saja perlu memakai `once` jika memang perlu hany sekali saja.

--- pramuat.js
+++ pramuat.js
@@ -3,6 +3,20 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('apiElectron', {
-  kirimPesan: (pesan) => ipcRenderer.send('pesan-dari-jendela', pesan),
-  terimaBalasan: (callback) => ipcRenderer.on('balasan-dari-utama', (event, data) => callback(data))
+  kirimPesan: (pesan, saluranBalasan) => {
+  ipcRenderer.send('pesan-dari-jendela', { pesan, saluranBalasan });
+  },
+
+  terimaPesan: (callback) => {
+    const listener = (acara, data) => callback(data);
+    ipcRenderer.on('pesan-dari-utama', listener);
+
+    return () => {
+      ipcRenderer.removeListener('pesan-dari-utama', listener);
+    };
+  },
+
+  kirimBalasan: (saluranBalasan, data) => {
+    ipcRenderer.send(saluranBalasan, data);
+  }
 });
