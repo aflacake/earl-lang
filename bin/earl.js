669ef1b7f36473608dcfc47a577c904262af4bce
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-08-23 18:28:35 +0700

    Menggunakan await
    `runEarl` adalah fungsi async, kalau tidak await maka eksekusi bisa terjadi tidak sinkron dan jika kode lain juga memanggilnya, jadi dobel.

--- bin/earl.js
+++ bin/earl.js
@@ -1,25 +1,28 @@
 #!/usr/bin/env node
 
 // bin/earl.js
+
 const fs = require('fs');
 const path = require('path');
 const { runEarl } = require('../index');
 
-const args = process.argv.slice(2);
+async function main() {
+    const args = process.argv.slice(2);
 
-if (args.length === 0) {
-    console.log("Gunakan: earl file.earl  atau  earl \"kode langsung\"");
-    process.exit(1);
+    if (args.length === 0) {
+        console.log("Gunakan: earl file.earl  atau  earl \"kode langsung\"");
+        process.exit(1);
+    }
+
+    const input = args[0];
+
+    if (input.endsWith('.earl') && fs.existsSync(input)) {
+        const code = fs.readFileSync(input, 'utf8');
+        await runEarl(code);
+    } else {
+        const code = input.replace(/\\n/g, '\n');
+        await runEarl(code);
+    }
 }
 
-const input = args[0];
-
-if (input.endsWith('.earl') && fs.existsSync(input)) {
-    const code = fs.readFileSync(input, 'utf8');
-    console.log('Kode yang akan dijalankan:', JSON.stringify(code));
-    runEarl(code).then(() => process.exit(0));
-} else {
-    const code = input.replace(/\\n/g, '\n');
-    console.log('Kode yang akan dijalankan:', JSON.stringify(code));
-    runEarl(code).then(() => process.exit(0));
-}
+main();
